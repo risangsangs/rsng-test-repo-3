876a2131381ce26f1671f7c41160b299c74d8ad7
risangsangs <sangagnijati@gmail.com>
2023-05-23 13:47:49 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World !</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus molestias dignissimos quibusdam mollitia libero obcaecati, quos doloribus sit totam inventore voluptas ducimus quaerat nobis optio eius praesentium voluptatum nihil. Explicabo!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
